3c5fbd68231474d05c8f77f455ef237110a826d7
putrilikaaaa <tempattugasnyael@gmail.com>
2024-10-03 05:37:54 -0700

    Update ta_003.py

--- ta_003.py
+++ ta_003.py
@@ -102,6 +102,9 @@ def pemetaan(data_df):
         # Ensure DTW distance matrix is symmetric
         dtw_distance_matrix_daily = symmetrize(dtw_distance_matrix_daily)
 
+        # **Check size of distance matrix and labels**
+        num_samples = dtw_distance_matrix_daily.shape[0]
+        
         # Clustering and silhouette score calculation for daily data
         max_n_clusters = 10
         silhouette_scores = {}
@@ -110,105 +113,114 @@ def pemetaan(data_df):
         for n_clusters in range(2, max_n_clusters + 1):
             clustering = AgglomerativeClustering(n_clusters=n_clusters, metric='precomputed', linkage=linkage_method)
             labels = clustering.fit_predict(dtw_distance_matrix_daily)
-            score = silhouette_score(dtw_distance_matrix_daily, labels, metric='precomputed')
-            silhouette_scores[n_clusters] = score
-            cluster_labels_dict[n_clusters] = labels
+
+            if len(labels) == num_samples:  # Ensure label size matches the distance matrix
+                try:
+                    score = silhouette_score(dtw_distance_matrix_daily, labels, metric='precomputed')
+                    silhouette_scores[n_clusters] = score
+                    cluster_labels_dict[n_clusters] = labels
+                except ValueError as e:
+                    st.error(f"Error calculating silhouette score: {e}")
+            else:
+                st.error("Jumlah label tidak sesuai dengan ukuran matriks jarak.")
 
         # Plot Silhouette Scores
-        plt.figure(figsize=(10, 6))
-        plt.plot(list(silhouette_scores.keys()), list(silhouette_scores.values()), marker='o', linestyle='-')
+        if silhouette_scores:
+            plt.figure(figsize=(10, 6))
+            plt.plot(list(silhouette_scores.keys()), list(silhouette_scores.values()), marker='o', linestyle='-')
 
-        # Adding data labels to the silhouette score plot
-        for n_clusters, score in silhouette_scores.items():
-            plt.text(n_clusters, score, f"{score:.2f}", fontsize=9, ha='right')
+            # Adding data labels to the silhouette score plot
+            for n_clusters, score in silhouette_scores.items():
+                plt.text(n_clusters, score, f"{score:.2f}", fontsize=9, ha='right')
 
-        plt.title('Silhouette Score vs. Number of Clusters (Data Harian)')
-        plt.xlabel('Number of Clusters')
-        plt.ylabel('Silhouette Score')
-        plt.xticks(range(2, max_n_clusters + 1))
-        plt.grid(True)
-        st.pyplot(plt)
+            plt.title('Silhouette Score vs. Number of Clusters (Data Harian)')
+            plt.xlabel('Number of Clusters')
+            plt.ylabel('Silhouette Score')
+            plt.xticks(range(2, max_n_clusters + 1))
+            plt.grid(True)
+            st.pyplot(plt)
 
         # Determine optimal number of clusters
-        optimal_n_clusters = max(silhouette_scores, key=silhouette_scores.get)
-        st.write(f"Jumlah kluster optimal berdasarkan Silhouette Score adalah: {optimal_n_clusters}")
+        if silhouette_scores:
+            optimal_n_clusters = max(silhouette_scores, key=silhouette_scores.get)
+            st.write(f"Jumlah kluster optimal berdasarkan Silhouette Score adalah: {optimal_n_clusters}")
+        
+            # Clustering and dendrogram
+            condensed_dtw_distance_matrix = squareform(dtw_distance_matrix_daily)
+            Z = linkage(condensed_dtw_distance_matrix, method=linkage_method)
 
-        # Clustering and dendrogram
-        condensed_dtw_distance_matrix = squareform(dtw_distance_matrix_daily)
-        Z = linkage(condensed_dtw_distance_matrix, method=linkage_method)
+            plt.figure(figsize=(16, 10))
+            dendrogram(Z, labels=data_daily.columns, leaf_rotation=90)
+            plt.title(f'Dendrogram Clustering dengan DTW (Data Harian) - Linkage: {linkage_method.capitalize()}')
+            plt.xlabel('Provinsi')
+            plt.ylabel('Jarak DTW')
+            st.pyplot(plt)
 
-        plt.figure(figsize=(16, 10))
-        dendrogram(Z, labels=data_daily.columns, leaf_rotation=90)
-        plt.title(f'Dendrogram Clustering dengan DTW (Data Harian) - Linkage: {linkage_method.capitalize()}')
-        plt.xlabel('Provinsi')
-        plt.ylabel('Jarak DTW')
-        st.pyplot(plt)
-
-        # Table of provinces per cluster
-        cluster_labels = cluster_labels_dict[optimal_n_clusters]
-        clustered_data = pd.DataFrame({
-            'Province': data_daily.columns,
-            'Cluster': cluster_labels
-        })
-
-        # Display cluster table
-        st.subheader("Tabel Provinsi per Cluster")
-        st.write(clustered_data)
-
-        # Load GeoJSON file from GitHub
-        gdf = upload_geojson_file()
-
-        if gdf is not None:
-            gdf = gdf.rename(columns={'Propinsi': 'Province'})  # Change according to the correct column name
-            gdf['Province'] = gdf['Province'].str.upper().str.replace('.', '', regex=False).str.strip()
-
-            # Calculate cluster from clustering results
-            clustered_data['Province'] = clustered_data['Province'].str.upper().str.replace('.', '', regex=False).str.strip()
-
-            # Rename inconsistent provinces
-            gdf['Province'] = gdf['Province'].replace({
-                'DI ACEH': 'ACEH',
-                'KEPULAUAN BANGKA BELITUNG': 'BANGKA BELITUNG',
-                'NUSATENGGARA BARAT': 'NUSA TENGGARA BARAT',
-                'D.I YOGYAKARTA': 'DI YOGYAKARTA',
-                'DAERAH ISTIMEWA YOGYAKARTA': 'DI YOGYAKARTA',
+            # Table of provinces per cluster
+            cluster_labels = cluster_labels_dict[optimal_n_clusters]
+            clustered_data = pd.DataFrame({
+                'Province': data_daily.columns,
+                'Cluster': cluster_labels
             })
 
-            # Remove provinces that are None (i.e., GORONTALO)
-            gdf = gdf[gdf['Province'].notna()]
+            # Display cluster table
+            st.subheader("Tabel Provinsi per Cluster")
+            st.write(clustered_data)
 
-            # Merge clustered data with GeoDataFrame
-            gdf = gdf.merge(clustered_data, on='Province', how='left')
+            # Load GeoJSON file from GitHub
+            gdf = upload_geojson_file()
 
-            # Set colors for clusters
-            gdf['color'] = gdf['Cluster'].map({
-                0: 'red',
-                1: 'yellow',
-                2: 'green',
-                3: 'blue',
-                4: 'purple',
-                5: 'orange',
-                6: 'pink',
-                7: 'brown',
-                8: 'cyan',
-                9: 'magenta'
-            })
-            gdf['color'].fillna('grey', inplace=True)
+            if gdf is not None:
+                gdf = gdf.rename(columns={'Propinsi': 'Province'})  # Change according to the correct column name
+                gdf['Province'] = gdf['Province'].str.upper().str.replace('.', '', regex=False).str.strip()
 
-            # Display provinces colored grey
-            grey_provinces = gdf[gdf['color'] == 'grey']['Province'].tolist()
-            if grey_provinces:
-                st.subheader("Provinsi yang Tidak Termasuk dalam Kluster:")
-                st.write(grey_provinces)
-            else:
-                st.write("Semua provinsi termasuk dalam kluster.")
+                # Calculate cluster from clustering results
+                clustered_data['Province'] = clustered_data['Province'].str.upper().str.replace('.', '', regex=False).str.strip()
 
-            # Plot map
-            fig, ax = plt.subplots(1, 1, figsize=(12, 10))
-            gdf.boundary.plot(ax=ax, linewidth=1, color='black')  # Plot boundaries
-            gdf.plot(ax=ax, color=gdf['color'], edgecolor='black', alpha=0.7)  # Plot clusters
-            plt.title("Pemetaan Provinsi Berdasarkan Kluster")
-            st.pyplot(fig)
+                # Rename inconsistent provinces
+                gdf['Province'] = gdf['Province'].replace({
+                    'DI ACEH': 'ACEH',
+                    'KEPULAUAN BANGKA BELITUNG': 'BANGKA BELITUNG',
+                    'NUSATENGGARA BARAT': 'NUSA TENGGARA BARAT',
+                    'D.I YOGYAKARTA': 'DI YOGYAKARTA',
+                    'DAERAH ISTIMEWA YOGYAKARTA': 'DI YOGYAKARTA',
+                })
+
+                # Remove provinces that are None (i.e., GORONTALO)
+                gdf = gdf[gdf['Province'].notna()]
+
+                # Merge clustered data with GeoDataFrame
+                gdf = gdf.merge(clustered_data, on='Province', how='left')
+
+                # Set colors for clusters
+                gdf['color'] = gdf['Cluster'].map({
+                    0: 'red',
+                    1: 'yellow',
+                    2: 'green',
+                    3: 'blue',
+                    4: 'purple',
+                    5: 'orange',
+                    6: 'pink',
+                    7: 'brown',
+                    8: 'cyan',
+                    9: 'magenta'
+                })
+                gdf['color'].fillna('grey', inplace=True)
+
+                # Display provinces colored grey
+                grey_provinces = gdf[gdf['color'] == 'grey']['Province'].tolist()
+                if grey_provinces:
+                    st.subheader("Provinsi yang Tidak Termasuk dalam Kluster:")
+                    st.write(grey_provinces)
+                else:
+                    st.write("Semua provinsi termasuk dalam kluster.")
+
+                # Plot map
+                fig, ax = plt.subplots(1, 1, figsize=(12, 10))
+                gdf.boundary.plot(ax=ax, linewidth=1, color='black')  # Plot boundaries
+                gdf.plot(ax=ax, color=gdf['color'], edgecolor='black', alpha=0.7)  # Plot clusters
+                plt.title("Pemetaan Provinsi Berdasarkan Kluster")
+                st.pyplot(fig)
 
 # Function to compute local cost matrix for DTW
 def compute_local_cost_matrix(data_df: pd.DataFrame) -> np.array:
